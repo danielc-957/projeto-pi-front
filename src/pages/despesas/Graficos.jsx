d248f246414a083352e2883fd9ae1565b3d82da2
Caio dos santos <64568292+CaioodosSantos@users.noreply.github.com>
2022-06-14 01:18:29 -0300

    commit bug

--- src/pages/despesas/Graficos.jsx
+++ src/pages/despesas/Graficos.jsx
@@ -1,22 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
+import { Area, Bar, CartesianGrid, ComposedChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
 import apiDeputados from '../../services/apiDeputados'
 import './graficos.css'
 
 const Graficos = () => {
  
+
   const params = useParams() 
 
-  const [grafico, setGrafico] = useState({})
+  const [grafico, setGrafico] = useState([])
+
+  const [data, setData] = useState([])
+
+  const [mes, setMes] = useState({})
+
+  const [total, setTotal] = useState()
 
   useEffect(()=>{
+    let temp = []; 
+    let x = 0
+    for (let index = 1; index < 13; index++) {
+      
+      apiDeputados.get('/deputados/' + params.id +'/despesas?mes='+index+'&ordem=ASC&ano=2019&itens=100').then(resultado=>{
+        setGrafico(resultado.data.dados)
+        console.log(resultado.data.dados)
+        console.log('entrei' + index)
+
+      async function somar() { 
         
-    apiDeputados.get('/deputados/' + params.id +'/despesas?mes=06&ordem=ASC&ano=2019').then(resultado=>{
-      setGrafico(resultado.data.dados)
-      console.log(resultado.data.dados)
-})
-},[])
+        for (let index1 = 0; index1 < grafico.length; index1++) {
+          const gasto = await Number(grafico[index1].valorDocumento) 
+          console.log(gasto)
+          x = x + gasto
+          
+          
+        }
+        console.log(x)
+        return x
+      }
+
+      const total = somar()
+         temp[index] = {"name":`mes ${index}`,"media": total/(grafico.length),"total": total}
+         
+        console.log(temp[index])
+  
+  })
+
+  
+      
+    }
+
+    setData(temp)
+
+},[total])
+
+console.log(data)
 
 
   return (
@@ -24,23 +63,27 @@ const Graficos = () => {
                 <h1>Balanceamento de despesas</h1>
     <div id='box'>
     <div id='info'>
-    <LineChart width={500} height={300} data={params.data}>
-    <XAxis dataKey="name"/>
-    <YAxis/>
-    <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-    </LineChart>
+    
+  <ComposedChart width={520} height={320} data={data}>
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <CartesianGrid stroke="#f5f5f5" />
+  <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+  <Bar dataKey="gastos" barSize={70} fill="#5645DA" />
+  <Line type="monotone" dataKey="media" stroke="#fff" />
+  </ComposedChart>
     
     </div>
         
-    <div class="container">
-    <div class="row">
-        <div class="col-sm">
+    <div className="container">
+    <div className="row">
+        <div className="col-sm">
                     Descrição
         </div>
 
-        <div class="col-sm">
+        <div className="col-sm">
          01/04/2022 á 30/07/2022
         </div>
     </div>
